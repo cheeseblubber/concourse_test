4bd00285c8331bdbf3597aedc2b8363bcfab98ec
Kam Leung <kamleung911@gmail.com>
2014-12-05 15:09:33 -0800

    change bucket.exist to bucket.exists
    Signed-off-by: Hari Kodungallur <hkodungallur@pivotal.io>

--- s3_config.rb
+++ s3_config.rb
@@ -5,6 +5,6 @@ s3 = AWS::S3.new
 myfile = "/tmp/myproduct-#{ENV['GO_PIPELINE_COUNTER']}.txt"
 key = File.basename(myfile)
 bucket = s3.buckets['gocd-concourse-test']
-bucket.exist?
+bucket.exists?
 bucket.objects[key].write(:file => myfile)
 puts "I AM FINISHED"
