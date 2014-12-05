83e502428abf66442490f3eb396b64b010779051
Kam Leung <kamleung911@gmail.com>
2014-12-05 15:07:05 -0800

    added bucket name
    Signed-off-by: Hari Kodungallur <hkodungallur@pivotal.io>

--- s3_config.rb
+++ s3_config.rb
@@ -4,5 +4,7 @@ s3 = AWS::S3.new
 
 myfile = "/tmp/myproduct-#{ENV['GO_PIPELINE_COUNTER']}.txt"
 key = File.basename(myfile)
+bucket = s3.buckets['gocd-concourse-test']
+bucket.exist?
 bucket.objects[key].write(:file => myfile)
 puts "I AM FINISHED"
