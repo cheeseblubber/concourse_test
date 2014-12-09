99909a218584bb49793cc2fb395e94d318491a63
Brenda Chan <brchan@pivotal.io>
2014-12-09 10:07:17 -0800

    Updates directory name.

--- create_dummy.sh
+++ create_dummy.sh
@@ -7,7 +7,7 @@ FILE=dummy-$VERSION.txt
 echo $FILE
 echo 'hello world' > $FILE
 
-cd artifact
+cd git-latest-artifact
 echo "https://s3.amazonaws.com/gocd-concourse-test/$FILE" > latest_artifact.txt
 git config --global user.email "kamleung911@gmail.com"
 git config --global user.name "Kam Leung"
